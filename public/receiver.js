2f447882422f51da60c088e2a4e1c263a5330e48
Krishnendu <krishnendu.kalyani1@gmail.com>
2021-12-30 20:27:04 +0530

    disconnect, alert, id

--- public/receiver.js
+++ public/receiver.js
@@ -1,25 +1,76 @@
-(function(){
-    let senderID;
-    const socket = io();
+let senderID;
+        
+        const socket = io();
+        
+        function generateID(){
+            return `${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}`;  
+        }
+        document.getElementById("receive-btn").addEventListener("click", function(){
 
-    function generateID(){
-        return `${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}`;  
-    }
-    document.getElementById("receive-btn").addEventListener("click", function(){
+            senderID = document.querySelector("#join-id").value;
 
-        senderID = document.querySelector("#join-id").value;
-
-        if(senderID.length == 0){
-            return;
-        }     
-        let joinID = generateID();
-        socket.emit("reveiver-join", {
-            uid:joinID,
-            sender_uid:senderID
+            if(senderID.length == 0){
+                return;
+            }     
+            let joinID = generateID();
+            console.log(senderID);
+            socket.emit("receiver-id-check", senderID)
+            socket.emit("reveiver-join", {
+                uid:joinID,
+                sender_uid:senderID
+            });
+            // if(invalidEl != 1){
+                document.querySelector(".createRoom").classList.remove("active");
+                document.querySelector(".fs-screen").classList.add("active"); 
+            // } 
+            socket.on("invalid", (data) => {
+                if(data == 1){
+                    alert("invalid id");
+                    location.reload();
+                }                
+            });     
         });
 
-        document.querySelector(".createRoom").classList.remove("active");
-        document.querySelector(".fs-screen").classList.add("active");
+        function exit_send(){
+            socket.emit("exit-sender", {
+                uid: senderID
+            });
+            location.reload();
+        }
 
-    })
-});
+        let fileShare = {};
+        socket.on("fs-meta", function(metadata){
+            fileShare.metadata = metadata;
+            fileShare.transmitted = 0;
+            fileShare.buffer = [];
+            let el = document.querySelector(".item");
+            el.innerHTML=`
+            <div class="progress">0%</div>
+            <div class="filename">${metadata.filename}</div>
+            `;
+            document.querySelector(".files-list").appendChild(el)
+
+            fileShare.progress_node = el.querySelector(".progress");
+
+            socket.emit("fs-start", {
+                uid:senderID
+            });
+        });
+
+        socket.on("fs-share", function(buffer){
+            fileShare.buffer.push(buffer);
+            fileShare.transmitted += buffer.byteLength;
+            fileShare.progress_node.innerHTML = Math.trunc(fileShare.transmitted / fileShare.metadata.total_buffer_size * 100) + "%";
+            if(fileShare.transmitted == fileShare.metadata.total_buffer_size){
+                download(new Blob(fileShare.buffer), fileShare.metadata.filename);
+                fileShare = {};
+            }else{
+                socket.emit("fs-start", {
+                    uid:senderID
+                });
+            }
+        });
+
+        socket.on("exit-rev", ()=>{
+            location.reload();
+        });
--- server.js
+++ server.js
@@ -7,9 +7,19 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 app.use(express.static(path.join(__dirname+'/')));
 
+var id = [];
+var recentId;
 io.on("connection", function(socket){
     socket.on("sender-join", function(data){
         socket.join(data.uid);
+        recentId = data.uid;
+        id.push(data.uid);
+    });
+    socket.on("receiver-id-check", ids =>{
+        if(!id.includes(ids)){
+            console.log("invalid id");
+            socket.emit("invalid", 1);
+        }
     });
     socket.on("reveiver-join", function(data){
         socket.join(data.uid);
@@ -23,6 +33,29 @@ io.on("connection", function(socket){
     });
     socket.on("file-raw", function(data){
         socket.in(data.uid).emit("fs-share", data.buffer);
+    });
+    socket.on("exit-receiver", (data) => {
+        socket.in(data.uid).emit("exit-rev");
+    });
+    socket.on("exit-sender", (data) => {
+        for (let i = 0; i < id.length; i++) {
+            if (id[i] == data.uid) {
+                const removedElements = id.splice(i, 1);
+                i--;
+                break;
+            }
+        }
+        socket.in(data.uid).emit("exit-send");
+    });
+
+    socket.on("disconnect", (data) => {
+        for (let i = 0; i < id.length; i++) {
+            if (id[i] == recentId) {
+                const removedElements = id.splice(i, 1);
+                i--;
+                break;
+            }
+        }
     });
 });
 
